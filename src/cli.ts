d30ff09887ba7c889a03ead8635b36126905ef40
ZhuChongLin <8210856@qq.com>
2025-03-11 14:54:58 +0800

    feat(cli): 添加欢迎信息
    - 在 initCLI 函数中添加了欢迎信息的打印
    - 引入了 blue 函数以支持蓝色文本显示

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,6 @@
 import { Command } from 'commander'
 import fs from 'fs-extra'
-import { red } from 'kolorist'
+import { blue, red } from 'kolorist'
 import { fileURLToPath } from 'node:url'
 import path from 'path'
 import { createProject } from './project.js'
@@ -12,6 +12,7 @@ const packageJson = fs.readJSONSync(path.resolve(__dirname, '../package.json'))
  * 初始化命令行程序
  */
 export function initCLI() {
+  console.log(blue(`欢迎使用 create-vitarx v${packageJson.version} 构建向导`))
   const program = new Command()
   program
     .name('create-vitarx')
